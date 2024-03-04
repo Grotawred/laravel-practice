a4a7766f8fb54ced3151aaefe2c9158f399f55fd
Grotawred <grishavlasko200@gmail.com>
2024-03-04 18:58:50 +0200

    init

---
+++ PhpKILLER/phpKiller/resources/js/app.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import HelloWorldComponent from './components/HelloWorldComponent';
+const app = new Vue({
+el: '#app',
+render: h => h(HelloWorldComponent)
+});
---
+++ example-project/app/Http/Controllers/TestController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function sum(){
+        $i = 0;
+        $sum = 0;
+        while($i<=100){
+            $sum += $i;
+            $i++;
+        }
+
+        return $sum;
+    }
+
+    public function user(){
+        dd('Grisha Vlasko');
+    }
+    public function users(){
+        dd('Grisha Vlasko 17Lyceum');
+    }
+
+    public function showInfo(){
+        return view('form', ['name1' => 'Alex', 'name2' => 'Den', 'name3' => 'Arthur']);
+    }
+}
