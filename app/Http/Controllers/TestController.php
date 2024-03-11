5bb19b61aad919359607ca1ea00ff774960bb4f2
Grotawred <grishavlasko200@gmail.com>
2024-03-11 18:57:25 +0200

    Form add

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function sum(){
+        $i = 0;
+        $sum = 0;
+        while($i<=100){
+            $sum += $i;
+            $i++;
+        }
+
+        return $sum;
+    }
+
+    public function user(){
+        dd('Grisha Vlasko');
+    }
+    public function users(){
+        dd('Grisha Vlasko 17Lyceum');
+    }
+
+    public function showInfo(){
+        return view('table', ['name1' => 'Alex', 'name2' => 'Den', 'name3' => 'Arthur']);
+    }
+
+    public function showAddInfoPage(){
+        return view('form');
+    }
+
+    public function addInfo(Request $request){
+        dd($request->all);
+    }
+}
---
+++ resources/views/form.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+@csrf
+<form method="post" action="admin/add">
+    <div class="form-group">
+        <label for="inputName">Name</label>
+        <input type="text" class="form-control" id="inputName" placeholder="Enter Name">
+    </div>
+    <div class="form-group">
+        <label for="inputDescription">Name</label>
+        <input type="text" class="form-control" id="inputDescription" placeholder="Enter Description">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+</body>
+</html>
